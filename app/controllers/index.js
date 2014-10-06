e2c4cb1fc4ccf4984a5b56c37d36c4b86cae0f12
Yair <yair.rodriguez@negoapps.com>
2014-10-06 01:04:24 -0500

    calendario

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,17 @@
-function doClick(e) {
-    alert($.label.text);
-}
+
+
+$.btnCalendario.addEventListener("click", function() {
+	
+var calendario = Alloy.createController('Calendario').getView();
+	calendario.open({
+		transition : Ti.UI.iPhone.AnimationStyle.CURL_DOWN
+	});
+	
+	
+	//Titanium.Platform.openURL('http://www.ipn.mx/SiteCollectionDocuments/Calendario-IPN/Calend_ESCOLAR_14-15.pdf');
+});
+
+
+
 
 $.index.open();
