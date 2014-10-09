51ddc5ed94ae0aa23a3bfdb84babe664b200cdc0
Yair <yair.rodriguez@negoapps.com>
2014-10-09 02:49:38 -0500

    botones

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,15 +3,19 @@
 $.btnCalendario.addEventListener("click", function() {
 	
 var calendario = Alloy.createController('Calendario').getView();
-	calendario.open({
-		transition : Ti.UI.iPhone.AnimationStyle.CURL_DOWN
-	});
+		calendario.open();
+	
 	
 	
 	//Titanium.Platform.openURL('http://www.ipn.mx/SiteCollectionDocuments/Calendario-IPN/Calend_ESCOLAR_14-15.pdf');
 });
 
 
+$.btnReglamento.addEventListener("click", function() {
+	
+	var reglamento = Alloy.createController('Reglamento').getView();
+	reglamento.open();
+});
 
 
 $.index.open();
