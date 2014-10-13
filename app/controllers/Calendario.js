9c83f716eeda994a0a998498d3cad0559d1c74a0
Yair <yair.rodriguez@negoapps.com>
2014-10-13 16:30:44 -0500

    return

--- app/controllers/Calendario.js
+++ app/controllers/Calendario.js
@@ -1,1 +1,12 @@
 var args = arguments[0] || {};
+
+
+
+
+$.btnregresar.addEventListener("click", function(){
+	
+	
+	var AbreApp = Alloy.createController("index").getView();
+	 AbreApp.open();
+	
+});
